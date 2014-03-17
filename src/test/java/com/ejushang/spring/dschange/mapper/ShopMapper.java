e95124c44d58c518bea1e1fc761f6b0ad27db587
amoszhou <amoszhou@foxmail.com>
2014-03-17 13:43:29 +0800

    测试部分代码提交

---
+++ src/test/java/com/ejushang/spring/dschange/mapper/ShopMapper.java
@@ -0,0 +1,16 @@
+package com.ejushang.spring.dschange.mapper;
+
+import com.ejushang.spring.dschange.bean.Shop;
+
+import java.util.List;
+
+/**
+ * User: Baron.Zhang
+ * Date: 14-1-7
+ * Time: 上午9:31
+ */
+public interface ShopMapper {
+
+    public List<Shop> findAllShop();
+
+}
